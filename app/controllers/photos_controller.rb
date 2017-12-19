c0f244bd9ab675891d6d12e9a8fd85a5d5dfca9e
Joyce Wong <wong.cy.joyce@gmail.com>
2017-12-20 00:25:05 +0800

    added CSS for photo index, new and form pages

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,39 @@
 class PhotosController < ApplicationController
+  before_action :set_photo, only: [:show, :destroy]
+
+  def index
+    @photos = Photo.all
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      redirect_to photo_path(@photo)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @photo.destroy
+    redirect_to photos_path
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:photo, :caption)
+  end
+
+  def set_message
+    @photo = Photo.find(params[:id])
+  end
 end
+
