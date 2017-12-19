f9ae0bd778334201b0346b0ab25316e241c4ef16
Joyce Wong <wong.cy.joyce@gmail.com>
2017-12-19 15:47:37 +0800

    added links for viewing previous/next messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,17 @@
 class Message < ApplicationRecord
   validates :name, :email, :message, presence: true
 
+  scope :next, lambda {|id| where("id > ?",id).order("id ASC") }
+  scope :previous, lambda {|id| where("id < ?",id).order("id DESC") }
+
   # Enables photo uploads
   mount_uploader :photo, PhotoUploader
+
+  def next
+    Message.next(self.id).first
+  end
+
+  def previous
+    Message.previous(self.id).first
+  end
 end
